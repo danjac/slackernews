a4b12bd926616bfe040d6462cdbf12e0e28cc150
Dan Jacob <danjac354@gmail.com>
2015-10-20 23:26:01 +0300

    Adding route helper

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule Slackernews.LayoutView do
   use Slackernews.Web, :view
+
+  def title do
+    Slackernews.Endpoint.config(:app_name)
+  end
+
+  def active_class(conn, path) do
+    current_path = Path.join(["/" | conn.path_info])
+    if path == current_path do
+      "active"
+    else
+      ""
+    end
+  end
 end
